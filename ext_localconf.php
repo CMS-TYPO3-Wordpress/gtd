09a57b94366ded00784229d6d291d0ef6c2a9973
phasenraum2010 <thomas@woehlke.org>
2016-04-01 12:33:54 +0200

    after modelling

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,16 +9,15 @@ call_user_func(
             'ThomasWoehlke.' . $extKey,
             'Frontendsimpleworklist',
             [
-                'ProjectController' => 'addNewRootProject, addNewChildProject, ',
-                'TaskController' => 'inbox, today, next, waiting, scheduled, someday, completed, trash, emptyTrash, transformTaskIntoProject, completeTask, undoneTask, setFocus, unsetFocus, getAllTasksForUser, changeTaskOrderId, changeTaskOrderIdByProject, addNewTaskToProject, addNewTaskToInbox'
+                'ProjectController' => 'addNewRootProject, addNewChildProject, moveProject, getAllProjects, getRootProjects',
+                'TaskController' => 'inbox, today, next, waiting, scheduled, someday, completed, trash, emptyTrash, transformTaskIntoProject, completeTask, undoneTask, setFocus, unsetFocus, getAllTasksForUser, changeTaskOrderId, changeTaskOrderIdByProject, addNewTaskToProject, addNewTaskToInbox',
+                'UserAccountController' => 'list'
             ],
             // non-cacheable actions
             [
-                'UserAccount' => 'update',
-                'UserMessage' => '',
-                'Context' => '',
-                'Project' => 'update, delete, ',
-                'Task' => 'update, '
+                'ProjectController' => 'addNewRootProject, addNewChildProject, moveProject, getAllProjects, getRootProjects',
+                'TaskController' => 'inbox, today, next, waiting, scheduled, someday, completed, trash, emptyTrash, transformTaskIntoProject, completeTask, undoneTask, setFocus, unsetFocus, getAllTasksForUser, changeTaskOrderId, changeTaskOrderIdByProject, addNewTaskToProject, addNewTaskToInbox',
+                'UserAccountController' => 'list'
             ]
         );
 
