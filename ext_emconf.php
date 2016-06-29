a3ffbcceda6ee78dfe2db89e89209c169ecbf1e4
phasenraum2010 <thomas@woehlke.org>
2016-06-29 15:07:30 +0200

    working on #16

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,13 +20,15 @@ $EM_CONF[$_EXTKEY] = [
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            't3sbootstrap' => '3.1.0-3.1.99',
+        ],
     ],
 ];
